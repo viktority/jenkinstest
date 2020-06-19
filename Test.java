597e4e79f9e9acc9a0f1bef0b00ceb805800b6d6
chukwuka01 <victorc@hardcorebiometric.com>
2020-06-19 15:59:21 +0100

    added

---
+++ Test.java
@@ -0,0 +1,11 @@
+
+
+import java.util.Properties;
+
+public class Test {
+    public static void main(String[] args) {
+        Properties pros = System.getProperties();
+        System.out.println("Startttttttttttttttttttt");
+        pros.list(System.out);
+    }
+}
